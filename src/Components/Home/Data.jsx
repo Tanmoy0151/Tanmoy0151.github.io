0c6bff83d0e2a70d2299ea4c3fd16848b4ed8647
Tanmoy0151 <royanupama762@gmail.com>
2023-06-23 23:08:21 +0530

    fix home section

--- src/Components/Home/Data.jsx
+++ src/Components/Home/Data.jsx
@@ -4,13 +4,13 @@ const Data = () => {
     return (
         <div className="home-data">
             <h1 className="home-title">Tanmoy Roy</h1>
-            <h3 className="home-subtitle">Full-Stack-Developer</h3>
+            <h3 className="home-subtitle">Full-Stack Web Developer</h3>
             <p className="home-description">
                 I am a full-stack developer with experience in developing applications and developing applications with React components and components that integrate with React components.
             </p>
-<a href="#contact" className="button button--flex">
-    Say hello to me
-</a>
+            <a href="#contact" className="button button--flex">
+                Say hello to me
+            </a>
         </div>
     )
 }
